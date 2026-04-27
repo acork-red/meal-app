c53605acac389e8acd8682365c5098689859fa63
Alexander Corkhill <a.corkhill@spinwise.com>
2026-04-27 14:06:05 +0300

    initial commit

---
+++ lib/supabase.ts
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient(
+  "https://qqyzxvyxvnghgpydzfrj.supabase.co/rest/v1/",
+  "sb_publishable_0YFqPsM-kxGgxN1G7wieVQ_pmNlsmTo"
+);
